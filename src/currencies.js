28dfd64558383e6c9d0b2c14c1afce9e01ffb9ee
Karolina Jasiówka <80458977+KarolinaJ33@users.noreply.github.com>
2023-02-02 13:15:22 +0100

    currency board created

---
+++ src/currencies.js
@@ -0,0 +1,20 @@
+export const currencies = [
+  {
+     short: "EUR",
+     name: "Euro"
+     rate: 4.7079,
+  },
+
+  {
+     short: "CZK",
+     name: "Korona czeska"
+     rate: 0.1980,
+  },
+
+  {
+     short: "NOK",
+     name: "Korona norweska"
+     rate: 0.4312,
+  },
+
+];
