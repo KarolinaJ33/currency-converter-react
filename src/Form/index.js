c96f3c8cd83e4b6785c27232598b72ac7e347b95
Karolina Jasiówka <80458977+KarolinaJ33@users.noreply.github.com>
2023-02-03 11:08:42 +0100

    interaction added

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,12 +1,46 @@
 import "./style.css";
 import { currencies } from "../currencies";
+import { useState } from "react";
 
-const Form = () => (
-<p>
-  <label>
-    Kwota: <input className="form__field" required type="number" min="0.01" step="any" /> PLN
-  </label>
-</p>
-);
+export const Form = ({ calculateResult, result}) => {
+  const [currency, setCurrency] = useState(currencies[0].short);
+  const [amount, setAmount] = useState("");
 
-export default Form;
+  const onSubmit =(event) => {
+    event.preventDefault();
+    calculateResult(currency, amount);
+  }
+
+  return (
+    <fieldset className="fieldset">
+    <legend> Kalkulator walut</legend>
+    <span className="fieldset__currency">Kwota:</span>
+    <input
+    value={amount}
+    onChange= {({target}) => setAmount(target.value)}
+    type="number"
+    step="0.1"
+    name="zl"
+    id="zl"
+    required
+    placeholder="Wpisz kwotę"
+    />
+    <span className="fieldset__currency">Waluta: </span>
+    <select
+    value= {currency}
+    onChange= {({target}) => setCurrency(target.value)}
+    >
+      {currencies.map((currency => (
+        <option
+        key={currency.short}
+        value={currency.short}
+        >
+          {currency.name}
+        </option>
+      )))}
+      </select>
+      <button>Przelicz</button>
+      
+    </fieldset>
+    );
+  };
