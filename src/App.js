dbc73cfadaa332bfd3be1cf5c0c2f7055d5dbce8
Karolina Jasiówka <80458977+KarolinaJ33@users.noreply.github.com>
2023-02-03 12:24:08 +0100

    formatted code and added css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState} from 'react';
+import { useState} from "react";
 import { currencies } from "./currencies";
 import { Form } from "./Form";
 
@@ -7,15 +7,15 @@ function App() {
 
   const calculateResult =(currency, amount) => {
     const rate = currencies
-      .find (({short}) => short === currency)
+      .find(({short}) => short === currency)
       .rate;
-  }
 
   setResult({
     sourceAmount: +amount,
     targetAmount: amount / rate,
     currency
   });
+}
  
    return (
      <Form
