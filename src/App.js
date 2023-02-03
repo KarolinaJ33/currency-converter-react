bb1738e036b025bef77e8563f4cc69edd97c85b1
Karolina Jasiówka <80458977+KarolinaJ33@users.noreply.github.com>
2023-02-03 10:06:38 +0100

    code formatting

--- src/App.js
+++ src/App.js
@@ -2,10 +2,6 @@ import React, { useState} from 'react';
 import { currencies } from "./currencies";
 import Form from "./Form";
 
-
-
-
-
 function App() {
   const [result, setResult] = useState();
 
