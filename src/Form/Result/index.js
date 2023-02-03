26584cb88b8275509eedba9d4802568140880b9d
Karolina Jasiówka <80458977+KarolinaJ33@users.noreply.github.com>
2023-02-03 11:32:59 +0100

    added Result

---
+++ src/Form/Result/index.js
@@ -0,0 +1,14 @@
+
+export const Result  = ({ result }) => (
+    <p className="fieldset__result">
+    {result !== undefined && (
+        <>
+        Kwota po przeliczeniu&nbsp;={" "}
+
+        <strong>
+        {result.targetAmount.toFixed(2)}&nbsp;{result.currency}
+        </strong>
+        </>
+    )}
+    </p>
+);
