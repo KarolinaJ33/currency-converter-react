bb1ddb3d662f22ddd3e4d08a4bbb0cc7a70849a7
Karolina Jasiówka <80458977+KarolinaJ33@users.noreply.github.com>
2023-02-16 11:03:03 +0100

    Added component clock

---
+++ src/Clock/index.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+import "./style.css";
+
+const formatDate = (date) => date.toLocaleString(undefined, {
+    weekday: "long",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    day: "numeric",
+    month: "long"
+});
+
+export const Clock = () => {
+    const [date, setDate] = useState(new Date());
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setDate(new Date());
+        }, 1000);
+
+        return () => {
+            clearInterval(intervalId);
+        };
+    }, []);
+
+    return (
+        <p className="clock">
+            Dzisiaj jest {" "}
+            {formatDate(date)}
+        </p>
+    )
+};  
