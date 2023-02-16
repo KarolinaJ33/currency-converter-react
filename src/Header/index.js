cf26c63fb78c6802fbaf4da5eda6e2634aba0ce7
Karolina Jasiówka <80458977+KarolinaJ33@users.noreply.github.com>
2023-02-16 11:05:17 +0100

    header moved to new component Header

---
+++ src/Header/index.js
@@ -0,0 +1,9 @@
+import "./style.css";
+
+const Header = () => (
+    <header className="header">
+        <h1 className="header__title">Kalkulator walut</h1>
+    </header>
+);
+
+export default Header;
