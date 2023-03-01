a3145fe861caa7a44eabc217918fd0af552523f1
Karolina Jasiówka <80458977+KarolinaJ33@users.noreply.github.com>
2023-03-01 09:05:35 +0100

    ThemeProvider added in Header

--- src/Header/styled.js
+++ src/Header/styled.js
@@ -1,10 +1,6 @@
 import styled from "styled-components";
 
 export const Name = styled.header`
-    text-align: center;
-    color: ${({ theme }) => theme.color.luckyPoint};
-    font-weight: bold;
-    font-size: 20px;
     padding: 10px;
 `;
 
@@ -12,7 +8,7 @@ export const Title = styled.h1`
     max-width: 700px;
     width: 100%;
     font-size: 40px;
-    color: white;
+    color: ${({ theme }) => theme.color.white};
     background-color:${({ theme }) => theme.color.purpleHeart};  
     border-radius: 10px;
     padding: 5px;
