8576c770faac33c148c51ce9c261170b73040db3
Karolina Jasiówka <80458977+KarolinaJ33@users.noreply.github.com>
2023-03-03 13:35:12 +0100

    updated App

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
-import { useState } from "react";
-import { currencies } from "./currencies";
 import { Form } from "./Form";
 import { Container } from "./Container/styled";
 import Header from "./Header";
 import Clock from "./Clock";
 
+
 function App() {
-  const [result, setResult] = useState();
-
-  const calculateResult = (currency, amount) => {
-    const rate = currencies.find(({ short }) => short === currency).rate;
-
-    setResult({
-      sourceAmount: +amount,
-      targetAmount: amount / rate,
-      currency,
-    });
-  }
-
   return (
     <Container>
       <Clock />
        <Header />
-       <Form result={result} calculateResult={calculateResult} />
+       <Form  />
     </Container>
   );
 }
