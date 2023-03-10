fb023133d3538672144e2c19424bb036e910f075
Karolina Jasiówka <80458977+KarolinaJ33@users.noreply.github.com>
2023-03-10 08:25:53 +0100

    const variable moved outside the function and formatted code

--- src/Form/useRatesData.js
+++ src/Form/useRatesData.js
@@ -1,32 +1,34 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 
+const API_URL = "https://api.exchangerate.host/latest?base=PLN";
+
 export const useRatesData = () => {
-    const [ratesData, setRatesData] = useState({
-        state: "loading",
-    });
-    
-    useEffect(() => {
-      const API_URL = "https://api.exchangerate.host/latest?base=PLN";
+  const [ratesData, setRatesData] = useState({
+    state: "loading",
+  });
 
-       const getRatesData = async () => {
-          try {
-             const { data: { rates, date } } = await axios.get(API_URL);
+  useEffect(() => {
+    const getRatesData = async () => {
+      try {
+        const {
+          data: { rates, date },
+        } = await axios.get(API_URL);
 
-             setRatesData({
-                state: "success",
-                rates,
-                date, 
-             });
-          } catch {
-              setRatesData({
-                 state: "error",
-             });
-          }
-       };
+        setRatesData({
+          state: "success",
+          rates,
+          date,
+        });
+      } catch {
+        setRatesData({
+          state: "error",
+        });
+      }
+    };
 
-      setTimeout(getRatesData, 1000);
-    }, []);
+    setTimeout(getRatesData, 1000);
+  }, []);
 
-    return ratesData;
+  return ratesData;
 };
