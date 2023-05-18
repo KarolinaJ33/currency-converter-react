cb7d53d6c60e8436ddafbe3f320d02a35851bcce
Karolina Jasiówka <80458977+KarolinaJ33@users.noreply.github.com>
2023-05-18 12:34:59 +0200

    formatted code

--- src/Clock/index.js
+++ src/Clock/index.js
@@ -2,17 +2,18 @@ import { useCurrentDate } from "./useCurrentDate";
 import { Time } from "./styled";
 
 const Clock = () => {
-  const date = useCurrentDate();
-  const formattedDate = date.toLocaleString("pl", {
-    weekday: "long",
-    hour: "2-digit",
-    minute: "2-digit",
-    second: "2-digit",
-    day: "numeric",
-    month: "long",
-  });
+    const date = useCurrentDate();
 
-  return <Time>Dzisiaj jest {formattedDate}</Time>;
+    const formattedDate = date.toLocaleString("pl", {
+        weekday: "long",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+        day: "numeric",
+        month: "long",
+    });
+
+    return <Time>Dzisiaj jest {formattedDate}</Time>;
 };
 
 export default Clock;
